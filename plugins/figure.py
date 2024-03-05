15f7e23b41e5c4f65921d292acdd9f6160c0c269
J <jozerclaver@gmail.com>
2024-03-05 11:19:39 -0700

    Added automatic figure numbering

--- plugins/figure.py
+++ plugins/figure.py
@@ -1,23 +1,35 @@
 __all__ = ['figure']
 
-FIGURE_PATTERN = r'^~fig\((?P<fig_class>[\s\S]+?)?[ \t]*\n(?P<fig_img>[\s\S]+?)\n(?P<fig_caption>[\s\S]+?)\n\)~[ \t]*$'
+FIGURE_PATTERN = r'^~fig\((?P<fig_class>[^\s]+?)?[\s]*\n(?P<fig_img>[\s\S]+?)\n(?P<fig_caption>[\s\S]+?)\n\)~[\s]*$'
 # ^~fig\(                       open figure. must be at start of line
-# (?P<fig_class>[\s\S]+?)?      class (final ? makes it optional)
-# [ \t]*\n                      look past spaces to newline
+# (?P<fig_class>[^\s]+?)?       class (final ? makes it optional)
+# [\s]*\n                      look past spaces to newline
 # (?P<fig_img>[\s\S]+?)\n       first line within is image
 # (?P<fig_caption>[\s\S]+?)\n   second line within is caption
 # \)~                           close figure
-# [ \t]*$                       look past spaces to newline
+# [\s]*$                       look past spaces to newline
 
 def parse_block_figure(block, m, state):
     img = m.group('fig_img')
     caption = m.group('fig_caption')
     figclass = m.group('fig_class')
+
+    figindex = state.env.get('nextfigindex')
+    if not figindex:
+        figindex = 1
+    state.env['nextfigindex'] = figindex + 1
+
     state.append_token({
         'type': 'block_figure',
         'children': [
             {'type': 'fig_img', 'text': img},
-            {'type': 'fig_caption', 'text': caption}
+            {
+                'type': 'fig_caption',
+                'text': caption,
+                'attrs': {
+                    'figindex': figindex
+                }
+            }
         ],
         'attrs': {
             'figclass': figclass
@@ -33,8 +45,8 @@ def render_block_figure(renderer, children, figclass):
 def render_fig_img(renderer, text):
     return text
 
-def render_fig_caption(renderer, text):
-    return '<p>' + text + '</p>'
+def render_fig_caption(renderer, text, figindex):
+    return '<p>Figure ' + str(figindex) + ': ' + text + '</p>'
 
 def figure(md):
     """
