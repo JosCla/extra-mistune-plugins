2cced5f57b3ede5637e67dcc74304f43fbb21de4
J <jozerclaver@gmail.com>
2024-03-19 17:43:21 -0600

    Made section plugin block instead of inline

--- plugins/section.py
+++ plugins/section.py
@@ -3,39 +3,39 @@ __all__ = ['section']
 SECTION_START_PATTERN = r'^~sec$'
 SECTION_END_PATTERN = r'^sec~$'
 
-def parse_inline_section_start(inline, m, state):
+def parse_block_section_start(block, m, state):
     state.append_token({
-        'type': 'inline_section_start'
+        'type': 'block_section_start'
     })
 
     return m.end()
 
-def parse_inline_section_end(inline, m, state):
+def parse_block_section_end(block, m, state):
     state.append_token({
-        'type': 'inline_section_end'
+        'type': 'block_section_end'
     })
 
     return m.end()
 
-def render_inline_section_start(renderer):
+def render_block_section_start(renderer):
     return '<section>\n'
 
-def render_inline_section_end(renderer):
+def render_block_section_end(renderer):
     return '</section>\n'
 
 def section(md):
     """
     A mistune plugin to support sections.
 
-    .. inline-section-start:: text
+    .. block-section-start:: text
         ~sec
 
-    .. inline-section-end:: text
+    .. block-section-end:: text
         sec~
     """
 
-    md.inline.register('inline_section_start', SECTION_START_PATTERN, parse_inline_section_start, before='link')
-    md.inline.register('inline_section_end', SECTION_END_PATTERN, parse_inline_section_end, before='link')
+    md.block.register('block_section_start', SECTION_START_PATTERN, parse_block_section_start, before='list')
+    md.block.register('block_section_end', SECTION_END_PATTERN, parse_block_section_end, before='list')
     if md.renderer and md.renderer.NAME == 'html':
-        md.renderer.register('inline_section_start', render_inline_section_start)
-        md.renderer.register('inline_section_end', render_inline_section_end)
+        md.renderer.register('block_section_start', render_block_section_start)
+        md.renderer.register('block_section_end', render_block_section_end)
